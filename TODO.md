61bf786e959e08fede0ad26484db377546766e50
Azam Gulov <gulovazam@gmail.com>
2024-06-19 13:34:25 +0500

    Initial commit

---
+++ TODO.md
@@ -0,0 +1,10 @@
+# TODO
+
+- [] Display Product Previews on main page
+- [] Filtering Tools:
+  - [] By Price
+  - [] By Brand
+  - [] By Category
+- [] Product Page
+- [] Shopping cart functionally - OPTIONAL
+- [] Write my own REST API - OPTIONAL
---
+++ src/routes/Router.tsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Root";
+import Home from "./Home";
+import ProductPage from "./ProductPage";
+import Cart from "./Cart";
+import ErrorElement from "./ErrorElement";
