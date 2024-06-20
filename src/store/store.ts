30e9d6afdf68bbc09a7aa6bcbdce4aab68958f20
Azam Gulov <gulovazam@gmail.com>
2024-06-20 16:27:30 +0500

    Add cart reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../features/apiSlice";
+import { cartSlice } from "../features/cartSlice";
 
 export const store = configureStore({
   reducer: {
+    cart: cartSlice.reducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
