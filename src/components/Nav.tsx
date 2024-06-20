f3953aa351d5b0934a1046ecb9ca0fb9bc3f316c
Azam Gulov <gulovazam@gmail.com>
2024-06-20 16:40:23 +0500

    Wrap links in a list
    change styling

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,9 +2,17 @@ import { NavLink } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <nav>
-      <NavLink to={"/"}>Home</NavLink>
-      <NavLink to={"cart"}>Cart</NavLink>
+    <nav className="sticky top-0 border-b border-gray-400 bg-white border-opacity-20">
+      <div className="max-w-main-width mx-auto">
+        <ul className="flex items-center gap-4 h-[60px]">
+          <li>
+            <NavLink to={"/"}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to={"cart"}>Cart</NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
