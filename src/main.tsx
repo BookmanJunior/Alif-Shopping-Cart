01f241faccd2bb235b8c834eb6612c8ccc25f355
Azam Gulov <gulovazam@gmail.com>
2024-06-20 14:46:41 +0500

    Wrap Router component with Redux Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Router from "./routes/Router";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  </React.StrictMode>
+);
