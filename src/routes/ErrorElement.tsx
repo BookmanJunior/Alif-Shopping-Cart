e5344466cedc6591a04b0b3b89d1cab64a47fa5d
Azam Gulov <gulovazam@gmail.com>
2024-06-20 14:47:44 +0500

    Handle router error

--- src/routes/ErrorElement.tsx
+++ src/routes/ErrorElement.tsx
@@ -1,1 +1,15 @@
-export default function ErrorElement() {}
+import { isRouteErrorResponse, useRouteError, Link } from "react-router-dom";
+
+export default function ErrorElement() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <main>
+        <h1>{error.status}</h1>
+        <h2>{error.statusText}</h2>
+        <Link to="/">Go Back</Link>
+      </main>
+    );
+  }
+}
