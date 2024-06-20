a0aac326cfcb5e4dc2ad2b42df702f1bc5979926
Azam Gulov <gulovazam@gmail.com>
2024-06-20 14:48:08 +0500

    Add routes

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -4,3 +4,29 @@ import Home from "./Home";
 import ProductPage from "./ProductPage";
 import Cart from "./Cart";
 import ErrorElement from "./ErrorElement";
+
+export default function Router() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorElement />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "product/:id",
+          element: <ProductPage />,
+        },
+        {
+          path: "cart",
+          element: <Cart />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
