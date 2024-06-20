f9af2cd1445d8984ef70e2f40114c3e85b8fa888
Azam Gulov <gulovazam@gmail.com>
2024-06-20 23:44:44 +0500

    Composed ProductPreview
    styled component

--- src/components/ProductPreview.tsx
+++ src/components/ProductPreview.tsx
@@ -1,1 +1,25 @@
-export default function ProductPreview() {}
+import { Link } from "react-router-dom";
+import { Product } from "../Types/Product";
+import ProductPrice from "./Product/ProductPrice";
+import ProductButtons from "./Product/ProductButtons";
+import ProductImage from "./Product/ProductImage";
+
+export default function ProductPreview({ product }: { product: Product }) {
+  return (
+    <div className="grid">
+      <Link to={`/product/${product.id}`} className="group">
+        <ProductImage
+          className="w-[150px]"
+          productImage={product.image}
+          productTitle={product.title}
+        />
+        <ProductPrice productPrice={product.price} />
+        <p className="group-hover:text-yellow-400">{product.title}</p>
+      </Link>
+      <ProductButtons
+        product={product}
+        className="justify-self-start self-end"
+      />
+    </div>
+  );
+}
