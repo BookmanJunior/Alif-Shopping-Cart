442d0625857458d7a068a031932b9441dd6f1bf2
Azam Gulov <gulovazam@gmail.com>
2024-06-20 22:54:25 +0500

    Wrap spinner in a container
    change styling

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,5 +1,7 @@
 export default function Spinner() {
   return (
-    <div className=" w-[40px] aspect-square rounded-[50%] border-4 border-t-yellow-500 animate-spin"></div>
+    <div className="min-h-[calc(100vh-60px)] grid place-content-center">
+      <div className=" w-[40px] aspect-square rounded-[50%] border-4 border-t-yellow-500 animate-spin"></div>
+    </div>
   );
 }
