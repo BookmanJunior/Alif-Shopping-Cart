aa0a95d8e9831e20ddf2a3fdc7785a28774cf6b8
Gulov Azam <gulovazam@gmail.com>
2024-06-23 21:45:11 +0500

    Add ErrorBody subcomponent
    replace ErrorElements return value with ErrorBody

--- src/routes/ErrorElement.tsx
+++ src/routes/ErrorElement.tsx
@@ -5,13 +5,24 @@ export default function ErrorElement() {
 
   if (isRouteErrorResponse(error)) {
     return (
-      <main className="min-h-[100vh] grid place-content-center text-center gap-2">
-        <h1 className="text-3xl font-bold">{error.status}</h1>
-        <h2 className="text-xl font-bold">{error.statusText}</h2>
-        <Link className="button inline-block" to="/">
-          Go Back
-        </Link>
-      </main>
+      <ErrorBody errorStatus={error.status} errorText={error.statusText} />
     );
   }
+}
+
+interface ErrorBody {
+  errorStatus?: number;
+  errorText: string;
+}
+
+function ErrorBody({ errorStatus, errorText }: ErrorBody) {
+  return (
+    <main className="min-h-[100vh] grid place-content-center text-center gap-2">
+      {errorStatus && <h1 className="text-3xl font-bold">{errorStatus}</h1>}
+      <h2 className="text-xl font-bold">{errorText}</h2>
+      <Link className="button inline-block" to="/">
+        Go Back
+      </Link>
+    </main>
+  );
 }
